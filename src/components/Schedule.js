e3da88c7f9949460f439aacd3ecf5f59bfa780f5
artobay <aliyarfatemi@yahoo.com>
2018-09-08 19:48:17 -0700

    Schedule page is completed and its ready to get merged

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Body, View } from "native-base";
 
 export default class Schedule extends Component {
   render() {
     return (
-      <Container style={{backgroundColor: '#F3F3F4'}}>
+      <ScrollView style={{flex:1}}>
 
         <View style={{alignItems: 'center', justifyContent: 'center'}}>
             <Text style={{fontSize: 22}}>
@@ -25,15 +26,15 @@ export default class Schedule extends Component {
               </Body>
             </CardItem>
           </Card>
-        </Content>
 
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+
+        <View style={{alignItems: 'center', justifyContent: 'center', marginTop: 10, marginBottom: 10}}>
             <Text style={{fontSize: 22}}>
               Friday , Oct 19th
             </Text>
           </View>
 
-        <Content padder>
+
           <Card>
             <CardItem header bordered style={{backgroundColor: '#f4bc42', height:30}}>
               <Text style={{color: 'white'}}>4:30PM - 10PM</Text>
@@ -46,11 +47,36 @@ export default class Schedule extends Component {
               </Body>
             </CardItem>
           </Card>
+
+          <Card>
+            <CardItem header bordered style={{backgroundColor: '#f4bc42', height:30}}>
+              <Text style={{color: 'white'}}>4:30PM - 10PM</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text>
+                REGISTRATION AND EXPO 25+ COMPANIES DISPLAYING THEIR PRODUCTS AND SERVICES! INCLUDES FOOD STATIONS, NO-HOST BARS! PLUS - DJ, KAROKE, DANCING, PHOTO BOOTH provided by the Charlotte IAHSP® Regional Chapter – the official 2018 HOST CHAPTER
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+
+          <Card>
+            <CardItem header bordered style={{backgroundColor: '#f4bc42', height:30}}>
+              <Text style={{color: 'white'}}>4:30PM - 10PM</Text>
+            </CardItem>
+            <CardItem bordered>
+              <Body>
+                <Text>
+                REGISTRATION AND EXPO 25+ COMPANIES DISPLAYING THEIR PRODUCTS AND SERVICES! INCLUDES FOOD STATIONS, NO-HOST BARS! PLUS - DJ, KAROKE, DANCING, PHOTO BOOTH provided by the Charlotte IAHSP® Regional Chapter – the official 2018 HOST CHAPTER
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+
         </Content>
 
-      
-
-      </Container>
+      </ScrollView>
     );
   }
 }
