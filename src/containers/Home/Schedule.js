4681af9d710670556d4f539a63e51c391cfd33f2
Gabe <djgtraxx@gmail.com>
2018-10-14 17:44:46 -0700

    i wrote a custom function that compares the device's current time, with the time listed on each item.  currently its not checking the date though, just the time.  But, I got it working.  Also, I had to add all the extra AM's and PM's that were missing on the time schedule, so it causing some of the arrows to get dropped to a new line

--- src/containers/Home/Schedule.js
+++ src/containers/Home/Schedule.js
@@ -5,52 +5,57 @@ import { Content, Card, CardItem, Text, Body, View, Accordion, Icon } from "nati
 export default class Schedule extends Component {
   state = {
     friday: [
-      { a: "9:00 - 9:15 AM | LEADERSHIP TEAM ", content: "Welcome & Overview by the Leadership Team" },
-      { a: "9:15 - 9:45 AM | JENNIE NORRIS ", content: "Journey to the Top by Jennie Norris" },
-      { a: "10:00 - 10:45 AM | FRED BERNS ", content: "Selling Methods of the Masters by Fred Berns" },
-      { a: "10:50 - 11:20 AM | NAHEED OBERFELD ", content: "Mindset Strategies to Boost Your Income by Naheed Oberfeld" },
+      { a: "9:00 AM - 9:15 AM | LEADERSHIP TEAM ", content: "Welcome & Overview by the Leadership Team" },
+      { a: "9:15 AM - 9:45 AM | JENNIE NORRIS ", content: "Journey to the Top by Jennie Norris" },
+      { a: "10:00 AM - 10:45 AM | FRED BERNS ", content: "Selling Methods of the Masters by Fred Berns" },
+      { a: "10:50 AM - 11:20 AM | NAHEED OBERFELD ", content: "Mindset Strategies to Boost Your Income by Naheed Oberfeld" },
       { a: "11:25 AM - 12:15 PM | JOANNE WEARY ", content: "ONE DAY DECORATING - How to turn this immediate gratification service into income by Joanne Weary" },
 
       { a: "BREAKOUT SESSIONS ", content: "ATTENDEES SELECT 2 – 1 per time slot (sign ups will be done online prior to conference)" },
 
-      { a: "1:15 - 1:50 PM | ANDREA CHENISI ", content: "Create a Winning Team and How to Maintain It by Andrea Chenisi" },
-      { a: "1:15 - 1:50 PM | JUNE CARTER ", content: "Timing Sanity, The 34 Minute Time Management Process by June Carter" },
-      { a: "1:15 - 1:50 PM | CATHERINE LEWIS BROWN ", content: "Pricing Strategies that Work by Catherine Lewis Brown" },
-      { a: "1:15 - 1:50 PM | MARGARET GEHR ", content: "Planning for the Successful Expansion of your Business by Margaret Gehr" },
-      { a: "1:55 - 2:30 PM | ANDREA CHENISI ", content: "Create a Winning Team and How to Maintain It by Andrea Chenisi" },
-      { a: "1:55 - 2:30 PM | JUNE CARTER ", content: "Timing Sanity, The 34 Minute Time Management Process by June Carter" },
-      { a: "1:55 - 2:30 PM | CATHERINE LEWIS BROWN ", content: "Pricing Strategies that Work by Catherine Lewis Brown" },
-      { a: "1:55 - 2:30 PM | MARGARET GEHR ", content: "Planning for the Successful Expansion of your Business by Margaret Gehr" },
+      { a: "1:15 PM - 1:50 PM | ANDREA CHENISI ", content: "Create a Winning Team and How to Maintain It by Andrea Chenisi" },
+      { a: "1:15 PM - 1:50 PM | JUNE CARTER ", content: "Timing Sanity, The 34 Minute Time Management Process by June Carter" },
+      { a: "1:15 PM - 1:50 PM | CATHERINE LEWIS BROWN ", content: "Pricing Strategies that Work by Catherine Lewis Brown" },
+      { a: "1:15 PM - 1:50 PM | MARGARET GEHR ", content: "Planning for the Successful Expansion of your Business by Margaret Gehr" },
+      { a: "1:55 PM - 2:30 PM | ANDREA CHENISI ", content: "Create a Winning Team and How to Maintain It by Andrea Chenisi" },
+      { a: "1:55 PM - 2:30 PM | JUNE CARTER ", content: "Timing Sanity, The 34 Minute Time Management Process by June Carter" },
+      { a: "1:55 PM - 2:30 PM | CATHERINE LEWIS BROWN ", content: "Pricing Strategies that Work by Catherine Lewis Brown" },
+      { a: "1:55 PM - 2:30 PM | MARGARET GEHR ", content: "Planning for the Successful Expansion of your Business by Margaret Gehr" },
 
       { a: "BREAKOUT SESSIONS ", content: "ATTENDEES SELECT 2 – 1 per time slot (sign ups will be done online prior to conference)" },
 
-      { a: "2:45 - 3:20 PM | KEVIN HTAIN ", content: "Working with the Media by Kevin Htain" },
-      { a: "2:45 - 3:20 PM | CHRISTOPHER CORDER ", content: "Mastering Consultations by Christopher Corder" },
-      { a: "2:45 - 3:20 PM | LAKEITHA SMITH ", content: "L-Suite Netwërk - Company Management by Lakeitha Smith" },
-      { a: "2:45 - 3:20 PM | KAT TROULINOS ", content: "Staging for Builders by Kat Troulinos" },
-      { a: "3:25 - 4:00 PM | KEVIN HTAIN ", content: "Working with the Media by Kevin Htain" },
-      { a: "3:25 - 4:00 PM | CHRISTOPHER CORDER ", content: "Mastering Consultations by Christopher Corder" },
-      { a: "3:25 - 4:00 PM | LAKEITHA SMITH ", content: "L-Suite Netwërk - Company Management by Lakeitha Smith" },
-      { a: "3:25 - 4:00 PM | KAT TROULINOS ", content: "Staging for Builders by Kat Troulinos" },
-      { a: "4:15 - 5:15 PM | KEYNOTE SPEAKERS - THE SCOTT BROTHERS ", content: "Keynote Speakers - The Scott Brothers" },
-      { a: "5:00 - 6:00 PM | MEET N GREET and BOOK SIGNING ", content: "Meet N Greet and Book Signing" },
-      { a: "7:15 - 10:30 PM | DINNER & AWARDS CEREMONY AND WWSSW® FUNDRAISER and RED CARPET GALA ", content: "Dinner & Awards Ceremony and WWSSW® Fundraiser and Red Carpet Gala" },
+      { a: "2:45 PM - 3:20 PM | KEVIN HTAIN ", content: "Working with the Media by Kevin Htain" },
+      { a: "2:45 PM - 3:20 PM | CHRISTOPHER CORDER ", content: "Mastering Consultations by Christopher Corder" },
+      { a: "2:45 PM - 3:20 PM | LAKEITHA SMITH ", content: "L-Suite Netwërk - Company Management by Lakeitha Smith" },
+      { a: "2:45 PM - 3:20 PM | KAT TROULINOS ", content: "Staging for Builders by Kat Troulinos" },
+      { a: "3:25 PM - 4:00 PM | KEVIN HTAIN ", content: "Working with the Media by Kevin Htain" },
+      { a: "3:25 PM - 4:00 PM | CHRISTOPHER CORDER ", content: "Mastering Consultations by Christopher Corder" },
+      { a: "3:25 PM - 4:00 PM | LAKEITHA SMITH ", content: "L-Suite Netwërk - Company Management by Lakeitha Smith" },
+      { a: "3:25 PM - 4:00 PM | KAT TROULINOS ", content: "Staging for Builders by Kat Troulinos" },
+      { a: "4:15 PM - 5:15 PM | KEYNOTE SPEAKERS - THE SCOTT BROTHERS ", content: "Keynote Speakers - The Scott Brothers" },
+      { a: "5:00 PM - 6:00 PM | MEET N GREET and BOOK SIGNING ", content: "Meet N Greet and Book Signing" },
+      { a: "7:15 PM - 10:30 PM | DINNER & AWARDS CEREMONY AND WWSSW® FUNDRAISER and RED CARPET GALA ", content: "Dinner & Awards Ceremony and WWSSW® Fundraiser and Red Carpet Gala" },
     ],    
     saturday: [
-      { a: "9:00 - 9:40 AM | BARB SCHWARZ ", content: "Inspirational Talk by Barb Schwarz" },
+      { a: "9:00 AM - 9:40 AM | BARB SCHWARZ ", content: "Inspirational Talk by Barb Schwarz" },
       { a: "9:45 AM - 12:45 PM | LHS DESIGNATION ", content: "Luxury Home Staging Designation" },
-      { a: "1:45 - 2:45 PM | JOSEPH HAECKER ", content: "IAHSP® and Product Integration by Joseph Haecker" },
-      { a: "2:50 - 3:30 PM | CARRIE LITTLE ", content: "Social Media Strategies by Carrie Little" },
-      { a: "3:45 - 4:15 PM | MARY JO CAMP ", content: "Design Trends by Mary Jo Camp" },
-      { a: "4:20 - 5:00 PM | SCOTT HANSEN ", content: "Outthink - Outmarket - Outsell Your competition - 5 Part Profit Formula for Getting New Clients and Doubling Revenues" },
-      { a: "5:00 - 5:30 PM | LEADERSHIP TEAM ", content: "The Leadership Team" },
+      { a: "1:45 PM - 2:45 PM | JOSEPH HAECKER ", content: "IAHSP® and Product Integration by Joseph Haecker" },
+      { a: "2:50 PM - 3:30 PM | CARRIE LITTLE ", content: "Social Media Strategies by Carrie Little" },
+      { a: "3:45 PM - 4:15 PM | MARY JO CAMP ", content: "Design Trends by Mary Jo Camp" },
+      { a: "4:20 PM - 5:00 PM | SCOTT HANSEN ", content: "Outthink - Outmarket - Outsell Your competition - 5 Part Profit Formula for Getting New Clients and Doubling Revenues" },
+      { a: "5:00 PM - 5:30 PM | LEADERSHIP TEAM ", content: "The Leadership Team" },
     ]
   }
+  
 
   _renderHeader(item, expanded) {
+
+    const itemIsActive = checkIfItemIsCurrentlyActive(item.a);
+    const activeHeaderColor = (itemIsActive) ? "#66AE73" : "#EAA825";
+
     return (
       <View
-        style={{ flexDirection: "row", padding: 10, justifyContent: "space-between", alignItems: "center", backgroundColor: "#EAA825" }}
+        style={{ flexDirection: "row", padding: 10, justifyContent: "space-between", alignItems: "center", backgroundColor: activeHeaderColor }}
       >
         <Text style={{ fontSize: 15, fontWeight: "500", color: "white" }}>
           {item.a}
@@ -137,5 +142,56 @@ export default class Schedule extends Component {
         </Content>
       </ScrollView>
     );
+  }
+}
+
+
+
+
+
+/* ------------------------------------------------------------  */
+/* HELPER FUNCTIONS */
+/* ------------------------------------------------------------  */
+const checkIfItemIsCurrentlyActive = (itemA) => {
+  //Split the time from the title
+  const myItemAarray = itemA.split("|");
+
+  //Get the time range from the 1st position of the array
+  let myTimeRange = String(myItemAarray[0]);
+  //trim whitespace from it
+  myTimeRange = myTimeRange.trim();
+
+  //split the time range into two different times
+  const myTimeRangeArray = myTimeRange.split("-");
+  let myStartTime = String(myTimeRangeArray[0]);
+  let myEndTime = String(myTimeRangeArray[1]);
+  myStartTime = myStartTime.trim();
+  myEndTime = myEndTime.trim();
+
+  const currentTime = new Date();
+  //const sysTime = currentTime.getTime();
+
+  let year = currentTime.getFullYear();
+  let month = currentTime.getMonth()+1;
+  let day = currentTime.getDate();
+
+  if (month < 10) {
+     month = "0" + month;
+  }
+
+  if (day < 10) {
+     day = "0" + day;
+  }
+
+  let calTime1 = month + "/" + day + "/" + year + " " + myStartTime;
+  let calTime2 = month + "/" + day + "/" + year + " " + myEndTime;
+
+  const calDate1 = new Date(calTime1).getTime();
+  const calDate2 = new Date(calTime2).getTime();
+
+  if ( (currentTime >= calDate1) && (currentTime <= calDate2) ) {
+    return true;
+  } else {
+    return false;
   }
 }
